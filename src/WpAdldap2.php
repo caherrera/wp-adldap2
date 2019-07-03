953c51757d52f345376221e8c2743b5aa8902555
Carlos Herrera <caherrerac@falabella.cl>
2019-07-03 08:54:07 -0400

    trying adding provider

--- src/WpAdldap2.php
+++ src/WpAdldap2.php
@@ -3,6 +3,7 @@
 namespace WpAdldap2;
 
 use Adldap\Adldap;
+use Adldap\Schemas\ActiveDirectory;
 
 class WpAdldap2 extends Adldap {
 
@@ -10,6 +11,7 @@ class WpAdldap2 extends Adldap {
 	 * {@inheritdoc}
 	 */
 	public function __construct( array $providers = [] ) {
+		parent::__construct();
 		$config = [
 			Settings::CONFIG_HOSTS    => Settings::getHosts(),
 			Settings::CONFIG_BASEDN   => (string) Settings::getBasedn(),
@@ -17,7 +19,26 @@ class WpAdldap2 extends Adldap {
 			Settings::CONFIG_PASSWORD => (string) Settings::getPassword(),
 			Settings::CONFIG_PORT     => (string) Settings::getPort(),
 		];
-		parent::__construct( [ 'default' => $config ] );
+		$config = array_merge( [
+			// Optional Configuration Options
+			'schema'         => ActiveDirectory::class,
+			'account_prefix' => '',
+			'account_suffix' => '',
+
+			'follow_referrals' => false,
+			'use_ssl'          => false,
+			'use_tls'          => false,
+			'version'          => 3,
+			'timeout'          => 5,
+
+			// Custom LDAP Options
+			'custom_options'   => [
+				// See: http://php.net/ldap_set_option
+				LDAP_OPT_X_TLS_REQUIRE_CERT => LDAP_OPT_X_TLS_HARD
+			]
+		], $config );
+
+		$this->addProvider( $config );
 	}
 
 }
