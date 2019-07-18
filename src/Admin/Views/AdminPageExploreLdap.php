146d5c6ec98a6f979162a1c88840f638082f6ffa
Carlos Herrera <caherrerac@falabella.cl>
2019-07-18 12:16:58 -0400

    Multiple Changes

--- src/Admin/Views/AdminPageExploreLdap.php
+++ src/Admin/Views/AdminPageExploreLdap.php
@@ -15,30 +15,27 @@ class AdminPageExploreLdap extends HtmlForm {
 
 	public function __toString() {
 		$this->print();
-		$this->add( $this->wrap( 'small{display:block};','style' ) );
+		$this->add( $this->wrap( 'small{display:block};', 'style' ) );
 
 		return parent::__toString();
 	}
 
 	public function print() {
 
-		$p     = new HtmlTag();
-		$map   = array_flip( array_filter( Settings::getMap() ) );
-		$list  = $this->getList();
-		$first = current( $list );
-		$thead = $p->thead( [
+		$p       = new HtmlTag();
+		$map     = array_flip( array_filter( Settings::getMap() ) );
+		$list    = $this->getList();
+		$first   = current( $list );
+		$first   = array_keys( $first );
+		$first[] = '#';
+		$thead   = $p->thead( [
 			$p->tr( array_map( function ( $ldap ) use ( $map, $p ) {
 				return $p->th( ( $map[ $ldap ] ?? '' ) . $p->small( $ldap ) );
-			}, array_keys( $first ) ) ),
+			}, $first ) ),
 		] );
-		$tbody = array_map( function ( $user ) use ( $p ) {
-			$tr = array_map( function ( $attr ) use ( $p ) {
-				return $p->td( $attr );
-			}, $user );
 
-			return $p->tr( $tr );
-		}, $list );
-		$tbody = $p->tbody( $tbody );
+
+		$tbody = $p->tbody( $this->printTbody($list) );
 
 		$this->add( $p->table( [
 			$thead,
@@ -64,6 +61,25 @@ class AdminPageExploreLdap extends HtmlForm {
 		$this->list = $list;
 
 		return $this;
+	}
+
+	public function printTbody( $list ) {
+		$tbody = [];
+		foreach ( $list as $index => $user ) {
+			$tbody[] = $this->printUser( $index + 1, $user );
+		}
+
+		return $tbody;
+	}
+
+	public function printUser( $index, $user ) {
+		$p  = new HtmlTag();
+		$tr = array_map( function ( $attr ) use ( $p ) {
+			return $p->td( $attr );
+		}, $user );
+		array_unshift( $tr, $p->td( $index ) );
+
+		return $p->tr( $tr );
 	}
 
 
