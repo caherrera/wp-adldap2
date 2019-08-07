dd390d14ce853ba08b59df532345c80b6e3b5058
Carlos Herrera <carlos.herrera@viajesfalabella.com>
2019-08-06 21:56:29 -0400

    registering admin_post action

--- src/Admin/Init.php
+++ src/Admin/Init.php
@@ -4,6 +4,7 @@
 namespace WpAdldap2\Admin;
 
 use WpAdldap2\Settings;
+use WpAdldap2\Migrators\ExportCsv;
 
 class Init {
 	const ADLDAP2 = 'AD-LDAP2';
@@ -20,6 +21,7 @@ class Init {
 		if ( is_admin() ) { // admin actions
 			add_action( 'admin_menu', [ $this, 'menu' ] );
 			add_action( 'admin_init', [ $this, 'registerSettings' ] );
+			add_action( 'admin_post_adldap2_export_wp_users', [ ExportCsv::factory(), 'export' ] );
 		}
 
 
@@ -50,6 +52,7 @@ class Init {
 		add_menu_page( self::ADLDAP2, self::ADLDAP2, 'edit_users', self::ADLDAP2, [ AdminSettings::factory(), 'settingsPage' ] );
 		add_submenu_page( self::ADLDAP2, 'Test', 'Test', 'edit_users', self::ADLDAP2 . '_test', [ AdminTestConn::factory(), 'settingsPage' ] );
 		add_submenu_page( self::ADLDAP2, 'Explore List', 'Explore List', 'edit_users', self::ADLDAP2 . '_Explore', [ AdminExploreLdap::factory(), 'settingsPage' ] );
+		add_submenu_page( self::ADLDAP2, 'Export Wordpress Users', 'Export Wordpress Users', 'edit_users', self::ADLDAP2 . '_Export', [ AdminExport::factory(), 'settingsPage' ] );
 	}
 
 
